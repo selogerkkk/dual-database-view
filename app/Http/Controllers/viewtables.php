7e608e298fb2fa396a196a05b9a5541164f8f212
selogerkkk <ruanhborges@gmail.com>
2023-11-07 11:00:58 -0300

    feat: passagem de dados das colunas que nao existem

--- app/Http/Controllers/viewtables.php
+++ app/Http/Controllers/viewtables.php
@@ -12,6 +12,17 @@ class viewtables extends Controller
         $dadosTabela1 = DB::connection('mysql')->table('tabeladeteste')->get();
         $dadosTabela2 = DB::connection('mysql_secondary')->table('tabelacliente')->get();
 
-        return view('viewtables', compact('dadosTabela1', 'dadosTabela2'));
+        $colunasTabela1 = DB::connection('mysql')->getSchemaBuilder()->getColumnListing('tabeladeteste');
+        $colunasTabela2 = DB::connection('mysql_secondary')->getSchemaBuilder()->getColumnListing('tabelacliente');
+
+        $colunasQueNaoExistemEmTabela1 = array_diff($colunasTabela2, $colunasTabela1);
+        $colunasQueNaoExistemEmTabela2 = array_diff($colunasTabela1, $colunasTabela2);
+
+        return view('viewtables', compact(
+            'dadosTabela1',
+            'dadosTabela2',
+            'colunasQueNaoExistemEmTabela1',
+            'colunasQueNaoExistemEmTabela2'
+        ));
     }
 }
