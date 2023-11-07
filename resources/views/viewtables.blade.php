40a25a24e6eedc1a97d803ffd5c012c791d91fa7
selogerkkk <ruanhborges@gmail.com>
2023-11-07 11:01:35 -0300

    feat: inserçao dinamica das colunas das tabelas

--- resources/views/viewtables.blade.php
+++ resources/views/viewtables.blade.php
@@ -1,46 +1,49 @@
 <div>
     <h2>Tabela 1</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Idade</th>
-                <th>Email</th>
-                <th>Sexo</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($dadosTabela1 as $row)
+    @if ($dadosTabela1->count() > 0)
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $row->nome }}</td>
-                    <td>{{ $row->idade }}</td>
-                    <td>{{ $row->email }}</td>
-                    <td>{{ $row->sexo }}</td>
+                    @foreach ($dadosTabela1[0] as $key => $value)
+                        <th>{{ $key }}</th>
+                    @endforeach
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    
+            </thead>
+            <tbody>
+                @foreach ($dadosTabela1 as $row)
+                    <tr>
+                        @foreach ($row as $value)
+                            <td>{{ $value }}</td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Nenhum dado disponível na Tabela 1.</p>
+    @endif
+
     <h2>Tabela 2</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Idade</th>
-                <th>Email</th>
-                <th>Sexo</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($dadosTabela2 as $row)
+    @if ($dadosTabela2->count() > 0)
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $row->nome }}</td>
-                    <td>{{ $row->idade }}</td>
-                    <td>{{ $row->email }}</td>
-                    <td>{{ $row->sexo }}</td>
+                    @foreach ($dadosTabela2[0] as $key => $value)
+                        <th>{{ $key }}</th>
+                    @endforeach
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    
+            </thead>
+            <tbody>
+                @foreach ($dadosTabela2 as $row)
+                    <tr>
+                        @foreach ($row as $value)
+                            <td>{{ $value }}</td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Nenhum dado disponível na Tabela 1.</p>
+    @endif
 </div>
