c2a769498d1bf5bc9b00c301c37c93a029aab443
selogerkkk <ruanhborges@gmail.com>
2023-11-13 11:35:04 -0300

    style: Ajustar formatação do código

--- app/Http/Controllers/viewtables.php
+++ app/Http/Controllers/viewtables.php
@@ -11,16 +11,16 @@ class viewtables extends Controller
     {
 
         $nomeTabela1 = 'bancoprincipal';
-        $nomeTabela2 = 'tabelacliente';
-
         $dadosTabela1 = DB::connection('mysql')->table($nomeTabela1)->get();
-        $dadosTabela2 = DB::connection('mysql_secondary')->table($nomeTabela2)->get();
-
         $colunasTabela1 = DB::connection('mysql')->getSchemaBuilder()->getColumnListing($nomeTabela1);
+
+        $nomeTabela2 = 'tabelacliente';
+        $dadosTabela2 = DB::connection('mysql_secondary')->table($nomeTabela2)->get();
         $colunasTabela2 = DB::connection('mysql_secondary')->getSchemaBuilder()->getColumnListing($nomeTabela2);
 
         $colunasQueNaoExistemEmTabela1 = array_diff($colunasTabela2, $colunasTabela1);
         $colunasQueNaoExistemEmTabela2 = array_diff($colunasTabela1, $colunasTabela2);
+
 
         return view('viewtables', compact(
             'dadosTabela1',
